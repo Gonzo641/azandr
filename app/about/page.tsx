f5ffafabbff1ccc6c2b0d097c7cd568143295aca
jonas.mouret <jonathangonz6413@outlook.fr>
2026-01-14 16:20:58 +0100

    Add some pictures

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,19 +6,17 @@ export default function page() {
     <>
       <ReactLenis root />
       <div className="flex flex-col gap-8 py-60">
-        {/* <div className="mx-auto w-full flex gap-8 px-2 py-4">
-          <img src="/images/img_01.png" alt="Image 01" className="aspect-5/7" />
-          <img src="/images/img_02.png" alt="Image 02" className="aspect-5/7" />
-        </div>
-        <div className="mx-auto w-full flex gap-8 px-2 py-4">
-          <img src="/images/img_03.png" alt="Image 03" className="aspect-5/7" />
-          <img src="/images/img_04.png" alt="Image 04" className="aspect-5/7" />
-        </div> */}
-
-          <h1>
-            About me
-          </h1>
-
+        <Image
+          src="/images/Azandr4.jpg"
+          alt="Paysage en arrière-plan"
+          fill
+          priority
+          className="-z-10 opacity-80"
+          quality={75}
+        />
+        <h1 className="p-8">
+          About me
+        </h1>
       </div>
     </>
   )
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image";
+
 export default function ContactPage() {
   return (
     <div className="flex h-svh w-full flex-col items-center justify-center gap-8 p-8">
+      <Image
+        src="/images/Azandr9.jpg"
+        alt="Paysage en arrière-plan"
+        fill
+        priority
+        className="-z-10 opacity-50"
+        quality={75}
+      />
       <h1>Contact</h1>
       <a href="mailto:contact@silhouette.dev" className="text-2xl">
         contact@bmlc.com
